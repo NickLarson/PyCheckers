658da335df9944deea8d5ff8e7eb53532ed07e7b
edmachine <edwardhkoeller@gmail.com>
2011-08-30 17:58:47 -0500

    added getLocation as well

--- board_class.py
+++ board_class.py
@@ -1,12 +1,13 @@
 ##Example: {1 : 1} would denote row 1, column 1
 ##Example 2: {{1 : 1}
-class board(dict):
+class board():
     """initializing a board class..."""
     def __init__(self, size):
+        self.board = {}
         for row in range(1,size+1):
-            self[row] = {}                  ##Set the board to be full of dictionaries
+            self.board[row] = {}                  ##Set the board to be full of dictionaries
             for column in range(1,size+1):
-                self[row][column] = "EMPTY" ##Initialize to empty
-                
+                self.board[row][column] = "EMPTY" ##Initialize to empty
+    def getLocation(self, row, col):
+        return self.board(row,col)
     
-a = board(10)
