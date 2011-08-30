568e3fede14efc05a0050040e3313cd36625c8ac
Nicholas Larson <lars4045@umn.edu>
2011-08-30 17:16:37 -0500

    Broken.  __init__ needs fixing.  The stand alone initialize works.  It's just getting the syntax to cooperate.

--- test/board_class.py
+++ test/board_class.py
@@ -9,26 +9,61 @@ class board(dict):
         except KeyError:
             value = self[item] = type(self)()
             return value
+    def __init__(self, rows):
+        for row in range(1,rows+1):
+            if row != (rows/2) and row != (rows/2)-1:         ##We need to keep track of fully empty rows
+                if row%2 == 0:                        ##Odd row, odd column vice versa
+                    for col in range(1,rows+1):
+                        if col%2 == 0:
+                            self[row][col] = '0'
+                        else:
+                            self[row][col] = 'X'
+                else:                             ##This does the inverse... just fills it in backward
+                    for col in range(1,rows+1):
+                        if col%2 == 0:
+                            self[row][col] = 'X'
+                        else:
+                            self[row][col] = '0'
+            else:
+                for col in range(1,rows+1):    ##This will fill in the blank middle space... thus the rows/2 and rows/2-1
+                    self[row][col] = '0'
+a = board(10)
 
-a = board()
-
-def initialize(board):
-    for row in range(1,11):
-        if row%2 == 0:
-            for column in range(1,11):
-                board[column][row] = 'X'
+#board[row][column]
+#This worked when I took init out, it's basically the same just not a member
+def initialize(board, rows):
+    blank = rows/2
+    blank2 = blank-1
+    print blank2
+    print blank
+    for row in range(1,rows+1):
+        if row != (rows/2) and row != (rows/2)-1:         ##We need to keep track of fully empty rows
+            if row%2 == 0:                        ##Odd row, odd column vice versa
+                for col in range(1,rows+1):
+                    if col%2 == 0:
+                        board[row][col] = '0'
+                    else:
+                        board[row][col] = 'X'
+            else:
+                for col in range(1,rows+1):
+                    if col%2 == 0:
+                        board[row][col] = 'X'
+                    else:
+                        board[row][col] = '0'
         else:
-            for column in range(1,11):
-                board[column][row] = '0'
+            ##print row == rows/2
+            for col in range(1,rows+1):
+                board[row][col] = '0'
 
+def printBoard(board, row):
+    if row <= 10:
+        print a[row]
+        printBoard(board, row+1)
 
-def printBoard(board):
-    for x in range(1,11):
-        print "----Row ", x, "----"
-        print board[x]
-
+printBoard(a, 1)
 
 ##This returns true where all of the positions are filled in with a piece (Denoted by 'X')
-for y in range(1,11):
-    if a[1][y] == 'X':
-        print "True at position", y, "Row 1"
+def test():
+    for y in range(1,11):
+        if a[1][y] == 'X':
+            print "True at position", y, "Row 1"
