bf83222cf5b5003b45f291b31b9e6fa26dcdb8f2
Nick Larson <lars4045@umn.edu>
2011-08-30 21:37:43 -0500

    Updated checkers class with Test2 test file.

--- checkers_class.py
+++ checkers_class.py
@@ -10,11 +10,21 @@ class checkers():
         self.board = new_board
         red_piece = piece_class.piece("red", "regular")
         black_piece = piece_class.piece("black", "regular")
-        for row in range(1,5):
+        for row in range(1,4): ##This is going to set up the first player's pieces
             for column in range(1,11):
                 if row%2 == 0 and column%2 != 0:
                     new_board.setLocation(column, row, red_piece)
+                    print red_piece.printPiece(), row, column
                 if row%2 != 0 and column%2 == 0:
                     new_board.setLocation(column, row, red_piece)
+                    print red_piece.printPiece(), row, column
+        for row in range(6,9): ##This is going to set up the first player's pieces
+            for column in range(1,11):
+                if row%2 == 0 and column%2 != 0:
+                    new_board.setLocation(column, row, red_piece)
+                    print black_piece.printPiece(), row, column
+                if row%2 != 0 and column%2 == 0:
+                    new_board.setLocation(column, row, red_piece)
+                    print black_piece.printPiece(), row, column    
              
 
---
+++ test2.py
@@ -0,0 +1,6 @@
+##newtest
+import piece_class
+import board_class
+import checkers_class
+
+test_board = checkers_class.checkers(10)
